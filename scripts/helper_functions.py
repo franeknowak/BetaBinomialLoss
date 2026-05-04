ed0900d256f36d8709bde38fa2d9e4240d364768
FranekN <fmaxnowak@gmail.com>
2026-05-04 17:34:11 +0100

    removed dead code from helper_functions

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -1,22 +1,6 @@
 import math
 import torch
 
-def get_parameter_groups(model, classifier_dict):
-    temporal_proc_params = []
-    head_params = []
-
-    if classifier_dict['temporal_processing'] == 'gated_pooling' and classifier_dict['head'] == 'evidential':
-        for name, p in model.named_parameters():
-            if not p.requires_grad:
-                continue
-            if name.startswith("heads"):
-                head_params.append(p)
-            elif name.startswith("pool"):
-                temporal_proc_params.append(p)
-    else: raise Exception("Functionality not implemented. Only option available is 'gated_pooling' and 'evidential'")
-
-    return temporal_proc_params, head_params
-
 def get_schedulers(optimizer, CONFIG, len_train_dataloader):
         # Necessary vars
         ACC_STEPS   = CONFIG['TRAIN']['GRADIENT_ACC_BATCH_SIZE'] // CONFIG['TRAIN']['BATCH_SIZE']
