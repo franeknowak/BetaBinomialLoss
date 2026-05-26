e377b105267bb06434c268ddb0434cb8f8dd352e
FranekN <fmaxnowak@gmail.com>
2026-05-26 14:24:12 +0100

    reduce head capacity from in_feats//2 to in_feats//4

--- scripts/model.py
+++ scripts/model.py
@@ -58,10 +58,10 @@ class EvidentialHead(nn.Module):
         super().__init__()
         self.fc = nn.Sequential(
             nn.LayerNorm(in_feats),
-            nn.Linear(in_feats, in_feats//2),
+            nn.Linear(in_feats, in_feats//4),
             nn.GELU(),
             nn.Dropout(p=p_dropout),
-            nn.Linear(in_feats//2, 2),
+            nn.Linear(in_feats//4, 2),
         )
 
     def forward(self, x):
@@ -73,10 +73,10 @@ class BCEHead(nn.Module):
         super().__init__()
         self.fc = nn.Sequential(
             nn.LayerNorm(in_feats),
-            nn.Linear(in_feats, in_feats//2),
+            nn.Linear(in_feats, in_feats//4),
             nn.GELU(),
             nn.Dropout(p=p_dropout),
-            nn.Linear(in_feats//2, 1),
+            nn.Linear(in_feats//4, 1),
         )
 
     def forward(self, x):
